bec7c14538b43a1dcd9f8f6c489cc6f6a708fc12
yujianxinnian <593161522@qq.com>
2024-01-09 21:35:54 +0800

    评估

--- evaluation_py/kitti_eval_tools.py
+++ evaluation_py/kitti_eval_tools.py
@@ -938,7 +938,7 @@ if __name__ == "__main__":
     warnings.filterwarnings('ignore')
 
     label_file = '/home/dell/PaperCode/data/ObjectDetection/kitti_Hqss/MD_KITTI/validation/label_2'
-    pred_file = '/home/dell/PaperCode/MyDetection/DS-LV-GGNN/preds/kitti/Car/test80'
+    pred_file = '/home/dell/PaperCode/MyDetection/DS-LV-GGNN/preds/kitti/Car/test79'
     # current_classes = [0, 1, 2]  # or ['Car', 'Pedestrian', 'Cyclist']
     current_classes = [0]
 
